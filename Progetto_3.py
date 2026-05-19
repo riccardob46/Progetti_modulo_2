0e8beb07d4a03108345fc591fe7a2044b28086c3
riccardob46 <riccardo.bertti1998@gmail.com>
2026-05-19 19:22:52 +0200

    Update Progetto_3.py

--- Progetto_3.py
+++ Progetto_3.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime, timedelta
 
-# --- PREPARAZIONE DATASET (Stessa logica di prima) ---
+# --- PREPARAZIONE DATASET ---
 def create_dataset():
     np.random.seed(42)
     data = []
